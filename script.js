5c94ea8668dbf681bd257fbe09e9be45c0447f31
Pranav Rajagopal <rkpranav2024@gmail.com>
2025-07-13 16:18:16 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,12 @@
+const text = "Hey, I'm Pranav 👋";
+let index = 0;
+
+function typeWriter() {
+  if (index < text.length) {
+    document.getElementById("typewriter").innerHTML += text.charAt(index);
+    index++;
+    setTimeout(typeWriter, 100);
+  }
+}
+
+window.onload = typeWriter;
